1b7d9b0d898568c0b548035e09cbae88d0d7af0b
unknown <sebastian-olthoff@gmx.de>
2024-06-11 14:07:32 +0200

    tryed new way for fetching filmnames by name

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 const apiKey = '118b52da111b99ddfc41016d95301f2d';
 const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=de-DE&page=1`;
 
-document.getElementById('submitbtn').addEventListener('click', () => {
-    const query = document.getElementById('searchInput').value;
-    searchMovies(query);
-});
-function searchMovies(query) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=de-DE&query=${encodeURIComponent(query)}&page=1`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            displayResults(data.results);
-        })
-        .catch(error => {
-            console.error('Fehler beim Abrufen der Daten:', error);
-        });
+
+async function fetchData(){
+
+    try{
+
+        const searchInput = document.getElementById("searchInput").value.toLowerCase();
+        const response = await fetch(`${url}${searchInput}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");}
+        
+        if(response.json, searchInput) {
+            response.json === searchInput;
+
+        const data = await response.json();
+        const movieImg = data.poster_path;
+        const imgElement = document.getElementById("MovieCoverPic");
+
+        imgElement.src = movieImg;
+        imgElement.style.display = "block";}
+        
+            console.log("No Movie with this Name in Database")
+    }
+    catch(error){
+        console.error(error);
+    }
 }
-console.log(searchMovies);
+
+
+
