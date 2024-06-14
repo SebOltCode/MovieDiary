2bc0f6cd5aa7b2ecda0ae3a5686ee8ecb6eea87c
konstantinminster <konstantin@minster.biz>
2024-06-14 13:24:37 +0200

    bugfix

--- modules/storage.js
+++ modules/storage.js
@@ -1,6 +1,6 @@
 export function addMovieToLocalStorage(key, value) {
   const data = JSON.parse(localStorage.getItem(key)) || [];
-  const existingMovie = data.find(movie => movie.id === value.id)
+  const existingMovie = data.find(movie => movie.ID === value.ID)
   if (!existingMovie) {
     data.push(value);
       localStorage.setItem(key, JSON.stringify(data));
