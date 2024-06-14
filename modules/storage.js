e37e94765e4c8a2bdd77db8c854b68950c0ebaa3
Yongqi Zhang <zhangyongqi7@gmail.com>
2024-06-14 12:17:45 +0200

    fix addMovieToLocalStorage

--- modules/storage.js
+++ modules/storage.js
@@ -1,5 +1,8 @@
 export function addMovieToLocalStorage(key, value) {
   const data = JSON.parse(localStorage.getItem(key)) || [];
-  data.push(value);
-  localStorage.setItem(key, JSON.stringify(data));
+  const existingMovie = data.find(movie => movie.id === value.id)
+  if (!existingMovie) {
+    data.push(value);
+      localStorage.setItem(key, JSON.stringify(data));
+  } 
 }
